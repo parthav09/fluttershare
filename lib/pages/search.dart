d2a2970836435c96819b2fc6cd5a1b0b299ec031
Parthav <parthavjoshi1718@gmail.com>
2020-06-27 12:58:14 +0530

    Added user search options

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -7,8 +9,54 @@ class Search extends StatefulWidget {
 
 class _SearchState extends State<Search> {
   @override
+  buildSearchField() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      title: TextFormField(
+        decoration: InputDecoration(
+          hintText: "Search for a user",
+          filled: true,
+          prefixIcon: Icon(Icons.account_box),
+          suffixIcon: IconButton(
+            icon: Icon(Icons.clear),
+            onPressed: () => print("Cleared"),
+          ),
+        ),
+      ),
+    );
+  }
+
+  buildNoContent() {
+    return Container(
+      child: Center(
+        child: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            SvgPicture.asset(
+              "assets/images/search.svg",
+              height: 300,
+            ),
+            Text(
+              "Find Users",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontStyle: FontStyle.italic,
+                color: Colors.white,
+                fontWeight: FontWeight.w600,
+                fontSize: 60.0,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   Widget build(BuildContext context) {
-    return Text('Search');
+    return Scaffold(
+      appBar: buildSearchField(),
+      body: buildNoContent(),
+    );
   }
 }
 
