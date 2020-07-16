cf1d93d283f2e647f0437767ee730784ecd72135
Parthav <parthavjoshi1718@gmail.com>
2020-07-16 17:15:07 +0530

    basic UI design upload page

--- lib/pages/upload.dart
+++ lib/pages/upload.dart
@@ -6,8 +6,39 @@ class Upload extends StatefulWidget {
 }
 
 class _UploadState extends State<Upload> {
+  Widget buildSplashScreen() {
+    return Container(
+      color: Theme.of(context).primaryColor.withOpacity(0.7),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.asset('assets/images/turtle.png', height: 260),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                color: Colors.amber,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Icon(Icons.edit),
+                    Text("  Let's Share"),
+                  ],
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Upload");
+    return buildSplashScreen();
   }
 }
